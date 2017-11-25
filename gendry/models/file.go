9ca48444b5926e13aed034a459832c9540320464
Danny Hadley <dadleyy@gmail.com>
2017-11-25 11:06:43 -0800

    correcting hound transgressions

--- gendry/models/file.go
+++ gendry/models/file.go
@@ -2,6 +2,7 @@ package models
 
 //go:generate marlowc -input ./file.go
 
+// File records provide a database lookup for persisted files on the filestore.
 type File struct {
 	ID       uint   `marlow:"column=id&autoIncrement=true"`
 	SystemID string `marlow:"column=system_id"`
--- gendry/models/project.go
+++ gendry/models/project.go
@@ -2,6 +2,7 @@ package models
 
 //go:generate marlowc -input ./project.go
 
+// Project records represent a set of reports - each with an auth token for management.
 type Project struct {
 	ID       uint   `marlow:"column=id&autoIncrement=true"`
 	Name     string `marlow:"column=name"`
